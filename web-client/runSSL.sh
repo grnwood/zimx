b40e56997b3ea72f13c72b0e33f476f628fcb9a8
Joe Greenwood <grnwood@gmail.com>
2025-12-22 20:51:35 -0600

    updates

--- web-client/runSSL.sh
+++ web-client/runSSL.sh
@@ -1,2 +1,2 @@
-DEV_SSL_CERT=../dev-assets/certs/local-cert.pem DEV_SSL_KEY=../dev-assets/certs/local-key.pem VITE_API_BASE_URL=https://192.168.1.77:8443 npm run dev -- --host
+DEV_SSL_CERT=../dev-assets/certs/local-cert.pem DEV_SSL_KEY=../dev-assets/certs/local-key.pem VITE_API_BASE_URL=https://192.168.1.171:8443 npm run dev -- --host
 
